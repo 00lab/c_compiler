e47f22ed0677e23ebc5a8c4c62d1f81582d1473a
qibao <ufofcz@163.com>
2023-06-01 21:14:11 +0800

    Update log.cpp

--- 0common/log.cpp
+++ 0common/log.cpp
@@ -5,24 +5,24 @@
 
 
 // class Printer;
-void Log::LogPrinter(char const *prefix, char const *levelStr, char const *func, char const *pcFmt, ...)
-{
+void Log::LogPrinter(char const *prefix, char const *levelStr, char const *func, char const *pcFmt, ...) {
   static Printer print;
-    int  iRet;
-    va_list pcArgs;
-    char acLogBuf[MAX_LOG_STR_SIZE] = {0};
+  int  iRet;
+  va_list pcArgs;
+  char acLogBuf[MAX_LOG_STR_SIZE] = {0};
 
-    /* 填充日志信息 */
-    va_start(pcArgs, pcFmt);
-    iRet = vsnprintf(acLogBuf, MAX_LOG_STR_SIZE - 1, (const char*)pcFmt, pcArgs);
-    va_end(pcArgs);
-    if (iRet < 0) {
-        return;
-    }
-
-    /* 写日志 */
-    print << prefix << levelStr << func << ":" << acLogBuf << print.NewLine();
+  /* 填充日志信息 */
+  va_start(pcArgs, pcFmt);
+  iRet = vsnprintf(acLogBuf, MAX_LOG_STR_SIZE - 1, (const char*)pcFmt, pcArgs);
+  va_end(pcArgs);
+  if (iRet < 0) {
+    print << prefix << levelStr << __func__ << ": 组建打印log失败" << print.NewLine();
     return;
+  }
+
+  /* 写日志 */
+  print << prefix << levelStr << func << ":" << acLogBuf << print.NewLine();
+  return;
 }
 
 // common printing function, will print current indent after a new line
@@ -57,3 +57,42 @@ void Printer::DecCurrentIndent() {
 }
 
 SpecialChar Printer::NewLine() { return SpecialChar::NEW_LINE; }
+
+
+void CodeErrInfo::PrintCodeLog(GetSrcFileInfoFuncType GetSrcFileInfo, char const *compName, char const *pcFmt, ...) {
+  char *fileName;
+  int row = 0, col = 0;
+  GetSrcFileInfo(&fileName, &row, &col);
+  int  iRet;
+  va_list pcArgs;
+  char logBuf[MAX_LOG_STR_SIZE] = {0};
+  /* 填充日志信息 */
+  va_start(pcArgs, pcFmt);
+  iRet = vsnprintf(logBuf, MAX_LOG_STR_SIZE - 1, (const char*)pcFmt, pcArgs);
+  va_end(pcArgs);
+  if (iRet < 0) {
+    LOG_ERR("组建打印log失败");
+    return;
+  }
+  print << CODE_ERR_PREFIX << compName << fileName << ":" << row << "行-" << col << logBuf << print.NewLine();
+}
+
+void CodeErrInfo::PrintCodeLog(char const *srcFileInfoStr, char const *compName, char const *pcFmt, ...) {
+  int  iRet;
+  va_list pcArgs;
+  char logBuf[MAX_LOG_STR_SIZE] = {0};
+  /* 填充日志信息 */
+  va_start(pcArgs, pcFmt);
+  iRet = vsnprintf(logBuf, MAX_LOG_STR_SIZE - 1, (const char*)pcFmt, pcArgs);
+  va_end(pcArgs);
+  if (iRet < 0) {
+    LOG_ERR("组建打印log失败");
+    return;
+  }
+  print << CODE_ERR_PREFIX << compName << srcFileInfoStr << logBuf << print.NewLine();
+}
+
+void CodeErrInfo::LexerErr(const char *srcFileInfoStr, const char *errMsg) {
+  PrintCodeLog(srcFileInfoStr, COMP_NM_LEXER, errMsg);
+  errNum++;
+}

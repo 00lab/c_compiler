939acd8126d11980fdb2dc250eda5d4f30240c3f
qibao <ufofcz@163.com>
2023-06-01 21:17:46 +0800

    Update main_lexer.cpp

--- 1lexer/main_lexer.cpp
+++ 1lexer/main_lexer.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include "log.h"
+#include "lexer.h"
+#include "token.h"
 
-int main() {
+int main(int argc, char **argv)
+{
   LOG_INFO("lexer start\n");
+  if (argc > 2) {
+    LOG_INFO("暂不支持多个文件输入");
+  }
+  Scanner sc(argv[1]);
+  Lexer lex(sc);
+  Token &t = *(lex.GetNextToken());
+  while (t.tag != TokenTag::END) {
+    LOG_INFO(t.ToString().c_str());
+    t = *(lex.GetNextToken());
+  }
 }

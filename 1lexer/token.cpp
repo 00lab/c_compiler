f2d0fe1cb978a8919f4a64eae15b77c8771ee563
qibao <ufofcz@163.com>
2023-06-02 11:55:29 +0800

    Update token.cpp

--- 1lexer/token.cpp
+++ 1lexer/token.cpp
@@ -1,27 +1,22 @@
 #include <sstream>
 #include "token.h"
 
-string TokenId::ToString()
-{
-	return Token::ToString() + name;
+string TokenId::ToString() {
+	return Token::ToString() + ":" + name;
 }
 
-string TokenStr::ToString()
-{
+string TokenStr::ToString() {
 	return string("[") + Token::ToString() + string("]") + val;
 }
 
-string TokenNum::ToString()
-{
+string TokenNum::ToString() {
 	stringstream ss;
 	ss << val;
 	return string("[") + Token::ToString() + string("]") + ss.str();
 }
 
-string TokenChar::ToString()
-{
+string TokenChar::ToString() {
 	string ret = string("[") + Token::ToString() + string("]");
 	ret.append((const char*)&val);
 	return  ret;
 }
-

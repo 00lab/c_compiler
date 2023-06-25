61154fcb2bb03de84cebe578df785a9b84a49f45
qibao <ufofcz@163.com>
2023-06-25 13:04:53 +0800

    Update symbol_table.cpp

--- 2parser/symbol_table.cpp
+++ 2parser/symbol_table.cpp
@@ -29,12 +29,42 @@ SymbolTable::SymbolTable() {
 }
 
 SymbolTable::~SymbolTable() {
-  
+  // 清除函数列表
+  unordered_map<string, SymFunc *>::iterator funcTabIt = functionTable.begin();
+  for (; funcTabIt != functionTable.end(); ++funcTabIt) {
+    delete funcTabIt->second;
+  }
+  // 清除变量表
+  unordered_map<string, vector<SymValue *> *>::iterator varTabIt = variableTable.begin();
+  for (; varTabIt != variableTable.end(); ++varTabIt) {
+    vector<SymValue *> &vList = varTabIt->second;
+    for (int i = 0; i < vList.size(); ++i) {
+      delete vList[i];
+    }
+    delete &vList;
+  }
+  // 清除字符串
+  unordered_map<string, SymValue *>::iterator strTabIt = stringTable.begin();
+  for (; strTabIt != stringTable.end(); ++strTabIt) {
+    delete strTabIt->second;
+  }
 }
 
   // 符号表作用域管理
-void SymbolTable::EnterNewScope(); // 进入局部作用域
-void SymbolTable::LeaveCurrScope(); // 离开局部作用域
+void SymbolTable::EnterNewScope() { // 进入局部作用域
+  scopeId++;
+  scopePath.push_back(scopeId);
+  if (currFunc != nullptr) {
+    currFunc->EnterScope();
+  }
+}
+
+void SymbolTable::LeaveCurrScope() { // 离开局部作用域
+  scopePath.pop_back(); // 撤销更改
+  if (currFunc != nullptr) {
+    currFunc->LeaveScope();
+  }
+}
 
   // 变量管理， 散列表方式管理
 SymbolTable::AddSymVal(SymValue *v) { // 添加一变量 到符号表
@@ -70,6 +100,28 @@ void SymbolTable::AddDefFuncEnd(); // 函数定义结束位置
 SymFunc *SymbolTable::GetSymFunc(string funcName, vector<SymValue *> &args); // 根据调用类型，获取一个函数
 // TODO add IR
 
-vector<int>& SymbolTable::GetScopePath(); // 获取作用域path, -1全局，0 main函数
-SymFunc *SymbolTable::GetCurrFunc(); // 获取当前正在分析的函数
-void SymbolTable::ToString();
+void SymbolTable::ToString() {
+  LOG_INFO("#####变量表#####");
+  for (int i = 0; i < variableList.size(); ++i) {
+    string varName = variableList[i];
+    vector<SymFunc *> &vList = *variableTable[varName];
+    LOG_INFO("name=%s:", varName);
+    for (int j = 0; j < vList.size(); ++j) {
+      LOG_INFO("\t%d: %s:", j, vList[j].ToString());
+    }
+  }
+  LOG_INFO("#####字符串表#####");
+  unordered_map<string, SymValue *>::iterator strTabIt = stringTable.begin();
+  for (int j = 0; strTabIt != stringTable.end(); ++strTabIt, ++j) {
+    LOG_INFO("%d: %s:", j, strTabIt->second->GetValStr().c_str());
+  }
+  LOG_INFO("#####函数表#####");
+  for (int i = 0; i < functionList.size(); ++i) {
+    string varName = variableList[i];
+    vector<SymFunc *> &vList = *variableTable[varName];
+    LOG_INFO("name=%s:", varName);
+    for (int j = 0; j < vList.size(); ++j) {
+      LOG_INFO("\t%d: %s:", j, vList[j].ToString());
+    }
+  }
+}

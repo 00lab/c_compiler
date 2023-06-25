84c53cd7b1868439b2a8bea1ae918d11ab9b0980
qibao <ufofcz@163.com>
2023-06-25 21:01:09 +0800

    Update common.h

--- 0common/common.h
+++ 0common/common.h
@@ -23,4 +23,7 @@ typedef void (*GetSrcFileInfoFuncType)(char **fileName, int *rowNum, int *colNum
 #define VAL_NAME_CHAR "<char>"
 
 #define ALIGNMENT_SIZE 4
+
+#define SCOPE_PATH_SIZE_GLOBAL 1
+
 #endif
